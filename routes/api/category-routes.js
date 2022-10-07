a1c67364a3e5bac8026a84c8772c2e9444faaf2a
Vivian Chen <vivian.chen1193@gmail.com>
2022-10-07 12:43:16 -0700

    added associations between the models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,19 +6,69 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then((data) => {
+    if(!data) {
+      console.log('No categories found');
+      return;
+    } else {
+      res.json(data);
+    }
+  })
+  .catch((err) => console.log(err))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then((data) => {
+    if(!data) {
+      console.log('No categories found')
+    } else {
+      res.json(data)
+    }
+  })
+  .catch((err) => console.log(err))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then((data) => res.json(data))
+  .catch((err) => console.log(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((data) => {
+    if(!data) {
+      console.log('No categories found')
+    } else {
+      res.json(data)
+    }
+  })
+  .catch((err) => console.log(err))
 });
 
 router.delete('/:id', (req, res) => {
